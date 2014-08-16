6f45aa3cdcdb1c9ea1e42b5875005eeb0b730779
Ryan Benedetti <rhino.j.benedetti@gmail.com>
2014-08-15 18:33:16 -0700

    Completed Lesson 10, part 2

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,7 @@
+class EventsController < ApplicationController
+
+  def index
+    @events =['Event 1', 'Event 2', 'Event 3']
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Events::Application.config.secret_key_base = '2f90a5b7f0aa3688085b5d76d71bd7abe126a132837958a6b7b6c79ff54fe9187c6424f85726d784c0ce2b6ea626507524c2c14fbb44e58d299f71ed6482334d'
